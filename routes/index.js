a061cf5fbf7e96722d854bccbc393503982ba3b6
Neil Kandalgaonkar <neilk@brevity.org>
2014-12-07 23:02:59 -0800

    first cut

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+var ortho = [
+  {
+    label: 'io.js / Flux / React.js',
+    since: 1417156083000
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'JavaScript Orthodoxy',
+                        ortho: ortho });
 });
 
 module.exports = router;
